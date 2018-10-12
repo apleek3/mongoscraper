5e32106a1e7902293619e0795ba097ee60cdfda8
apleek3 <apleek3@gmail.com>
2018-10-11 18:37:15 -0700

    major stylings and legal advisories added

--- public/app.js
+++ public/app.js
@@ -1,47 +1,82 @@
-// Returns the articles as a json
-$.getJSON("/articles", function(data) {
-  for (var i = 0; i < data.length; i++) {
-    // Displays the information on the page
-    $("#articles").append(
-      "<p data-id='" +
-        data[i]._id +
-        "'>" +
-        data[i].title +
-        "<br />" +
-        data[i].link +
-        "</p>"
-    );
-  }
+// // Returns the articles as a json
+// $.getJSON("/articles", function(data) {
+//   for (var i = 0; i < data.length; i++) {
+//     // Displays the information on the page
+//     $("#articles").append(
+//       "<p data-id='" +
+//         data[i]._id +
+//         "'>" +
+//         data[i].title +
+//         "<br />" +
+//         data[i].link +
+//         "</p>"
+//     );
+//   }
+// });
+
+$(document).ready(function() {
+  console.log("READY!");
+  $("#articles").empty(); // Empty the notes section after submit
+  // Returns the articles as a json
+  $.getJSON("/articles", function(data) {
+    for (var i = 0; i < data.length; i++) {
+      // Displays the information on the page
+      $("#articles").append(
+        "<p data-id='" +
+          data[i]._id +
+          "'>" +
+          data[i].title +
+          "<br />" +
+          data[i].link +
+          "</p>"
+      );
+    }
+  });
+
+  $("#notes").empty(); // Empty the notes section after submit
+  // Returns the articles as a json
+  $.getJSON("/articles/..", function(data) {
+    for (var i = 0; i < data.length; i++) {
+      // Displays the information on the page
+      $("#articles").append(
+        "<p data-id='" +
+          data[i].title +
+          "<br />" +
+          data[i].body +
+          "</p>"
+      );
+    }
+  });
 });
 
 $(document).on("click", "p", function() {
-  $("#notes").empty();  // Empty the notes from the note section
-  var thisId = $(this).attr("data-id");  // Save the id from the p tag
+  $("#notes").empty(); // Empty the notes from the note section
+  var thisId = $(this).attr("data-id"); // Save the id from the p tag
 
   // ARTICLE ajax call
   $.ajax({
     method: "GET",
     url: "/articles/" + thisId
-  })  .then(function(data) {
-      console.log(data);
-      // TITLE
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      // Title input
-      $("#notes").append("<input id='titleinput' name='title' >");
-      // TEXT AREA
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // SUBMIT BUTTON
-      $("#notes").append(
-        "<button data-id='" + data._id + "' id='savenote'>Save Note</button>"
-      );
+  }).then(function(data) {
+    console.log(data);
+    // TITLE
+    $("#notes").append("<h2>" + data.title + "</h2>");
+    // Title input
+    $("#notes").append("<input id='titleinput' name='title' >");
+    // TEXT AREA
+    $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+    // SUBMIT BUTTON
+    $("#notes").append(
+      "<button data-id='" + data._id + "' id='savenote'>Save Note</button>"
+    );
 
-      if (data.note) {
-        // Modifies the title based on the title input
-        $("#titleinput").val(data.note.title);
-        // Modifies the body based on text input
-        $("#bodyinput").val(data.note.body);
-      }
-    });
+    if (data.note) {
+      // Modifies the title based on the title input
+      $("#titleinput").val(data.note.title);
+      // Modifies the body based on text input
+      $("#bodyinput").val(data.note.body);
+    }
+  });
 });
 
 // SAVE BUTTON for the notes
@@ -58,10 +93,10 @@ $(document).on("click", "#savenote", function() {
       // TEXT INPUT
       body: $("#bodyinput").val()
     }
-  })  .then(function(data) {
-      console.log(data); // For confirmation
-      $("#notes").empty(); // Empty the notes section after submit
-    });
+  }).then(function(data) {
+    console.log(data); // For confirmation
+    $("#notes").empty(); // Empty the notes section after submit
+  });
 
   // Clears the values for new entries
   $("#titleinput").val("");
